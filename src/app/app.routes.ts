0868f803a3f37e19a783994e0a4654a31da910b3
matthias <19991503+mGloerfeld@users.noreply.github.com>
2024-08-20 11:19:04 +0200

    add wildcard pathes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppShellComponent } from './app-shell/app-shell.component';
 
 export const routes: Routes = [
-
+    { path: 'home', loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent) },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'shell', component: AppShellComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HelloWorldComponent } from './hello-world.component';
+import { HomeComponent } from './home.component';
 
-describe('HelloWorldComponent', () => {
-  let component: HelloWorldComponent;
-  let fixture: ComponentFixture<HelloWorldComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HelloWorldComponent]
+      imports: [HomeComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HelloWorldComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
