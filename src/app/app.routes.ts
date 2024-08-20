29deb2e2abdc29ee5b79f1c4141de9fd2c29e9bd
matthias <19991503+mGloerfeld@users.noreply.github.com>
2024-08-20 11:20:46 +0200

    reorder routes and comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { AppShellComponent } from './app-shell/app-shell.component';
 
 export const routes: Routes = [
     { path: 'home', loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent) },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'shell', component: AppShellComponent },
-    { path: '**', component: PageNotFoundComponent }
+    { path: 'shell', component: AppShellComponent },       // placeholder path for`app-shell features`
+    { path: '', redirectTo: 'home', pathMatch: 'full' },   // redirect to `home-component`
+    { path: '**', component: PageNotFoundComponent }       // Wildcard route for a 404 page
 ];
